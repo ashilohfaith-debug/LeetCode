961acfd70b3b1568505d330f1ee534a3d6aec9da
Shiloh Faith <ashilohfaith@gmail.com>
2026-07-02 18:56:53 +0530

    Update bruteForce.java

--- Day003/bruteForce.java
+++ Day003/bruteForce.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int r[] =new int[2];
+        int r[] = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int n = nums[i];
             for (int j = 0; j < nums.length; j++) {
