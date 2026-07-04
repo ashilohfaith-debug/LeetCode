5d9f73d0c7048aa4c6dab09dbd38719b0e887934
Shiloh Faith <ashilohfaith@gmail.com>
2026-07-04 11:20:09 +0530

    Update groupAnagrams.java

--- Day004/groupAnagrams.java
+++ Day004/groupAnagrams.java
@@ -1,11 +1,13 @@
-public class Solution {
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] charArray = s.toCharArray();
+            char charArray[] = s.toCharArray();
             Arrays.sort(charArray);
             String sortedS = new String(charArray);
-            map.putIfAbsent(sortedS, new ArrayList<>());
+            if (!map.containsKey(sortedS)) {
+                map.put(sortedS, new ArrayList<>());
+            }
             map.get(sortedS).add(s);
         }
         return new ArrayList<>(map.values());
