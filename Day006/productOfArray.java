f0b1d9f801bcf44d17a3386ee2ed600fa19fd8af
Shiloh Faith <ashilohfaith@gmail.com>
2026-07-08 18:34:11 +0530

    Update productOfArray.java

--- Day006/productOfArray.java
+++ Day006/productOfArray.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] nums1 = new int[nums.length];
+        int prod = 1, countZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                prod = prod * nums[i];
+            }
+            else
+                countZero++;
+        }
+        if (countZero > 1) {
+            return new int[nums.length];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (countZero == 0) {
+                nums1[i] = prod / nums[i];
+            }
+            else if (countZero == 1) {
+                nums1[i] = (nums[i] == 0) ? prod : 0;
+            }
+        }
+        return nums1;
+    }
+}
